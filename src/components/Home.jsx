78d77ab2c278f1763f08eccaa85f5a100abc5480
TANUJ KUMAR SAINI <tanujkumarsaini46@gmail.com>
2022-10-21 00:21:36 +0530

    homepage changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,40 @@ import { Button, Card, CardBody, CardFooter, Col, Container, Row, UncontrolledCa
 import Base from "./Base";
 
 const Home=({title,description,buttonName,myFun})=>{
+  
+ let styleOb={
+  padding:'20px',
+  background:'#e2e2e2',
+  border:'1px solid blue',
+  margin:'10px'
+
+};
+
     return(
       <Base>
-      <div>
-        <h1>This is homepage</h1>
-      </div>
+      
+ <div>
+        <UncontrolledCarousel className="carousel-inner tales" 
+  items={[
+    {
+    
+      key: 1,
+      src: 'https://github.com/tanujkumarsaini/my_shop/blob/main/public/images/c4.jpg?raw=true',
+      
+    },
+    {
+      key: 2,
+      src: 'https://github.com/tanujkumarsaini/my_shop/blob/main/public/images/ip2.jpg?raw=true'
+    },
+    {
+      key: 3,
+      src: 'https://github.com/tanujkumarsaini/my_shop/blob/main/public/images/l1.jpg?raw=true'
+    }
+  ]}
+ />
+
+        </div>
+
 </Base>
 
     ) ;
